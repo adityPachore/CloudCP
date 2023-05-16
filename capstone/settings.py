5118e357878362a1dc15d96c6b7d46897d42514c
Aditya Changdev Pachore <aditya.pachore20@vit.edu>
2023-05-16 23:18:17 +0530

    Update settings.py

--- capstone/settings.py
+++ capstone/settings.py
@@ -76,11 +76,21 @@ WSGI_APPLICATION = 'capstone.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+#DATABASES = {
+#   'default': {
+#       'ENGINE': 'django.db.backends.sqlite3',
+#       'NAME': BASE_DIR / 'db.sqlite3',
+#   }
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cloudcp',
+        'HOST': 'cloudcp.cta7msotpn32.ap-south-1.rds.amazonaws.com',
+        'USER': 'admin',
+        'PASSWORD': PASSWORD,
+        'PORT': '3306',
+    },
 }
 
 
